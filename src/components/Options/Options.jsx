dd19a64bf11105850618cbc00dd95d389eb89bb2
Sergii Volodarskyi <47503455+svolodarskyi@users.noreply.github.com>
2024-12-19 09:48:27 -0700

    Update Options.jsx: add conditional rendering of reset button

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,12 +1,16 @@
 import s from './Options.module.css';
 
-const Options = ({ onFeedbackGiven }) => {
+const Options = ({ onFeedbackGiven, totalFeedback }) => {
   return (
     <div className={s.options}>
       <button onClick={() => onFeedbackGiven('good')}>Good</button>
       <button onClick={() => onFeedbackGiven('neutral')}>Neutral</button>
       <button onClick={() => onFeedbackGiven('bad')}>Bad</button>
-      <button onClick={() => onFeedbackGiven('reset')}>Reset</button>
+      {totalFeedback ? (
+        <button onClick={() => onFeedbackGiven('reset')}>Reset</button>
+      ) : (
+        false
+      )}
     </div>
   );
 };
